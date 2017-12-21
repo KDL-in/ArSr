0b65dbcb8802e593ee69933e3e3a53d7c682b2ab
linkunda <mcwarmsun@hotmail.com>
2017-12-21 11:18:29 +0800

    preparation fin mainUI ing

--- app/src/main/java/com/arsr/arsr/MainActivity.java
+++ app/src/main/java/com/arsr/arsr/MainActivity.java
@@ -1,16 +1,64 @@
 package com.arsr.arsr;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends BasicActivity {
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //状态栏更换
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //滑动菜单
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);//利用返回按钮做导航按钮
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+    }
 
+    /**
+     * 加载右上角菜单
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    /**
+     * 右上角菜单点击事件
+     * 1. todo 后面再对各种项监听
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.settings:
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
